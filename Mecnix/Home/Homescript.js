63e4dc71b01ac24fefe9496657c55c4cca96f456
Priyanshu-27 <99650183+Priyanshu-27@users.noreply.github.com>
2022-09-21 00:17:43 +0530

    Add files via upload
    First project which i did for my client when i was  a beginner that time , its so much fun to do work for your first client .

---
+++ Mecnix/Home/Homescript.js
@@ -0,0 +1,28 @@
+// When click burger is down 
+
+let slide_donw = document.getElementsByClassName("media_nav");
+let hidden = document.getElementById("hidden");
+
+slide_down = () => {
+    if (slide_donw[0].style.display == "none") {
+        slide_donw[0].style.display = "block"
+        hidden.style.display = "none"
+    }
+    else {
+        slide_donw[0].style.display = "none"
+        hidden.style.display = "block"
+    }
+
+}
+
+cross_hide = () =>{
+    if (slide_donw[0].style.display == "block"){
+        slide_donw[0].style.display = "none"
+        hidden.style.display = "block"
+    }
+    else {
+        slide_donw[0].style.display = "flex"
+        hidden.style.display = "none"
+    }
+
+}
